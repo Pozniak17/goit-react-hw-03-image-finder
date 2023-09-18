bccac35cfe1abed43a34b2c04f74ede94caf1bf3
Yevhenii Pozniak <jekamanu@gmail.com>
2023-09-18 12:50:51 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { Component } from 'react';
 import { Seachbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Loader } from './Loader/Loader';
+import { Button } from './Button/Button';
 // import { Grid } from 'react-loader-spinner';
 
 const KEY = '28544484-259b47bf7f7000ebfc4f498cb';
@@ -13,36 +15,28 @@ export class App extends Component {
     images: [],
     page: 2,
     largeImage: '',
-    // isLoading: false,
+    isLoading: false,
     // error: null,
     // showModal: true,
   };
 
   async componentDidUpdate(prevProps, prevState) {
     const { query, page } = this.state;
-    const prevName = prevState.query;
 
-    try {
-      // перевіряємо попереднє і поточне ім'я
-      if (prevName !== query) {
-        // this.setState({ isLoading: true });
-
+    // перевіряємо попереднє і поточне ім'я
+    if (page !== prevState.page || query !== prevState.query) {
+      try {
+        this.setState({ isLoading: true });
         const response = await axios.get(
-          `https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12
-`
+          `https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
         );
         this.setState({ images: response.data.hits });
+      } catch (error) {
+        this.setState({ error });
+      } finally {
+        this.setState({ isLoading: false });
       }
-    } catch (error) {
-      this.setState({ error });
-      // } finally {
-      // this.setState({ isLoading: false });
     }
-
-    // if (
-    // this.state.page !== prevState.page ||
-    // this.state.query !== prevState.query
-    // )
   }
 
   handleFormSubmit = query => {
@@ -54,16 +48,14 @@ export class App extends Component {
   };
 
   render() {
-    const { images } = this.state;
+    const { images, isLoading, error } = this.state;
     // console.log(images);
     return (
       <>
+        {error && <h1>Whoops, something went wrong: {error.message}</h1>}
         <Seachbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery data={images} />
-
-        {/* {error && <p>Whoops, something went wrong: {error.message}</p>} */}
-        {/* {isLoading && <div>Загружае...</div>} */}
-        {/* {images.length > 0 && <ImageGallery data={images} />} */}
+        {isLoading ? <Loader /> : <ImageGallery data={images} />}
+        <Button>Load more</Button>
       </>
     );
   }
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,13 @@
-import { Grid } from 'react-loader-spinner';
+import { ColorRing } from 'react-loader-spinner';
 
-export const Loader = () => {
-  return (
-    <Grid
-      height="80"
-      width="80"
-      color="#4fa94d"
-      ariaLabel="grid-loading"
-      radius="12.5"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
-  );
-};
+export const Loader = () => (
+  <ColorRing
+    visible={true}
+    height="780"
+    width="1380"
+    ariaLabel="blocks-loading"
+    wrapperStyle={{}}
+    wrapperClass="blocks-wrapper"
+    colors={['#58070c', '#6b0362', '#6af8d5', '#000000', '#0fcc28']}
+  />
+);
